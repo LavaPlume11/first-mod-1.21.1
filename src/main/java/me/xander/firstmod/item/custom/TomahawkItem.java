8b41152b5c2da55fef493c3a24de9c33d1eb04eb
LavaPlume11 <tristamarisa@gmail.com>
2024-12-06 21:49:26 -0500

    fixed

--- src/main/java/me/xander/firstmod/item/custom/TomahawkItem.java
+++ src/main/java/me/xander/firstmod/item/custom/TomahawkItem.java
@@ -2,8 +2,10 @@ package me.xander.firstmod.item.custom;
 
 import me.xander.firstmod.entity.custom.TomahawkProjectileEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.AxeItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ToolMaterial;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.stat.Stats;
@@ -11,9 +13,11 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
 
-public class TomahawkItem extends Item {
-    public TomahawkItem(Settings settings) {
-        super(settings);
+public class TomahawkItem extends AxeItem {
+
+
+    public TomahawkItem(ToolMaterial toolMaterial, Settings settings) {
+        super(toolMaterial, settings);
     }
 
     @Override
@@ -23,7 +27,7 @@ public class TomahawkItem extends Item {
                 SoundCategory.NEUTRAL,0.5f,0.4f / (world.getRandom().nextFloat() * 0.4f + 0.8f));
         if (!world.isClient) {
             TomahawkProjectileEntity diceProjectile = new TomahawkProjectileEntity(world, user);
-            diceProjectile.setVelocity(user.getPitch(), user.getYaw(),0.0f,1.5f,0f);
+            diceProjectile.setVelocity(user, user.getPitch(), user.getYaw(),0.0f,2.0f,0f);
             world.spawnEntity(diceProjectile);
         }
 
